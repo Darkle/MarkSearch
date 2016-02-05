3b94c0d3c42707cd9ff6494be0c579aca089bc34
Coop <coop.coding@gmail.com>
2016-02-05 14:14:20 +1100

    Friday, 5 February 2016 at 2:14:12 PM
    Clean up comments in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,6 @@ gulp.task('default', function(callback) {
   )
 })
 
-//http://www.browsersync.io/docs/options/
 gulp.task('browser-sync', () =>
   browserSync.init({
     proxy: "localhost:3000",
@@ -38,14 +37,7 @@ gulp.task('browser-sync', () =>
     port: 3020,
     open: false, // Stop the browser from automatically opening
     notify: false,
-    //reloadDelay: 3000,
-    //reloadDebounce: 3000,
-    /****
-     * online: false makes it load MUCH faster
-     * http://www.browsersync.io/docs/options/#option-online
-     * note: This is needed for some features, so disable this if anything breaks
-     */
-    online: false,  //
+    online: false,  //online: false makes it load MUCH faster
     ghostMode: false  //dont want to mirror clicks, scrolls, forms on all devices
   })
 )
