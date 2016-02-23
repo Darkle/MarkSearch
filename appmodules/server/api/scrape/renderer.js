cc9584511c8643b4ac60ad5ec42d53b7b4174163
Coop <coop.coding@gmail.com>
2016-02-23 17:28:31 +1100

    renderer.js comment update

--- appmodules/server/api/scrape/renderer.js
+++ appmodules/server/api/scrape/renderer.js
@@ -95,7 +95,7 @@ module.exports = function () {
           /****
            * Update the urlToScrape to the new redirected location so we can
            * re-check it against oldURL in next redirect (if it happens). We
-           * need to do this check as resources on page can emit a redirect event.
+           * need to do this check as resources on page can also emit a redirect event.
            * We just want to make sure we dont get into an infinite redirect loop.
            * Using url.parse to add a trailing slash just in case.
            */
