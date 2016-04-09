6039963ecdd8ee72b64089ecfe325fb247fff295
Coop <coop.coding@gmail.com>
2016-04-09 15:03:47 +1000

    deletePage minor formatting

--- appmodules/server/api/deletePage.js
+++ appmodules/server/api/deletePage.js
@@ -4,16 +4,16 @@ var pagesdb = require('../../db/pagesdb')
 var appLogger = require('../../utils/appLogger')
 
 function deletePage(req, res, next) {
-      pagesdb
-        .deleteRow(req.params.pageUrl)
-        .then( numRowsAffected => {
-          res.status(200).end()
-        })
-        .catch( err => {
-          console.error(err)
-          appLogger.log.error({err, req, res})
-          res.status(500).end()
-        })
+  pagesdb
+    .deleteRow(req.params.pageUrl)
+    .then( numRowsAffected => {
+      res.status(200).end()
+    })
+    .catch( err => {
+      console.error(err)
+      appLogger.log.error({err, req, res})
+      res.status(500).end()
+    })
 }
 
 module.exports = deletePage
