3ad46f6d2e15cc80623ce3ed86dba4b99b4acfab
Coop <coop.coding@gmail.com>
2016-04-17 21:19:25 +1000

    Updated sqlite3 build gulp task

--- gulpTasks/buildSqlite3LibBindings.js
+++ gulpTasks/buildSqlite3LibBindings.js
@@ -4,12 +4,7 @@ var path = require('path')
 
 var gulp = require('gulp')
 var exeq = require('exeq')
-var symlink = require('fs-symlink')
-var runSequence = require('run-sequence')
-var jetpack = require('fs-jetpack')
-var semver = require('semver')
 
-var basePath = path.resolve('')
 var platform = process.platform
 
 gulp.task('sqlite', () => {
@@ -17,36 +12,7 @@ gulp.task('sqlite', () => {
   var sqliteBinaryFullDir
 
   if(platform === 'darwin'){
-    /****
-     * OSX - go through the '/usr/local/Cellar/sqlite/' folder
-     * and use semver to get the most up to date version installed
-     * (homebrew doesn't usually uninstall the old versions).
-     */
-    var osxSqliteBinaryBaseDir = '/usr/local/Cellar/sqlite/'
-
-    try{
-
-      var sqliteVersions = jetpack.list('/usr/local/Cellar/sqlite/')
-
-      console.log('OSX sqlite versions available')
-      console.log(sqliteVersions)
-
-      var osxVersionToUse = '0.0.0'
-
-      sqliteVersions.forEach(version => {
-        if(semver.gt(version, osxVersionToUse)){
-          osxVersionToUse = version
-        }
-      })
-
-      console.log('osxVersionToUse')
-      console.log(osxVersionToUse)
-
-      sqliteBinaryFullDir = osxSqliteBinaryBaseDir + osxVersionToUse
-    }
-    catch(err){
-      console.error(`There was an error getting the osxSqliteBinaryFullDir`, err)
-    }
+    sqliteBinaryFullDir = '/usr/local/opt/sqlite/'
   }
   // else if(platform === 'darwin'){
   //
@@ -55,27 +21,16 @@ gulp.task('sqlite', () => {
   //
   // }
 
+  var electronVersion = require(path.resolve('', 'package.json')).dependencies['electron-prebuilt'].slice(1)
+
   return exeq(
-    `npm install sqlite3 --save --build-from-source --sqlite=${ sqliteBinaryFullDir }`
+    `npm install sqlite3 --runtime=electron --target=${ electronVersion } --build-from-source --sqlite=${ sqliteBinaryFullDir }`
   )
   .then(function() {
     console.log('Successfully compiled sqlite3 lib binding')
-    return runSequence('sqliteSymlink')
   })
   .catch(function(err) {
     console.error('There was an error building sqlite3 lib binding', err)
   })
 
-})
-
-gulp.task('sqliteSymlink', () => {
-  var nodeBindingDefaultFolder = path.join(basePath, 'node_modules', 'sqlite3', 'lib', 'binding', 'node-v47-darwin-x64')
-  var nodeBindingSymlink = path.join(basePath, 'node_modules', 'sqlite3', 'lib', 'binding', 'electron-v0.37-darwin-x64')
-  return symlink(nodeBindingDefaultFolder, nodeBindingSymlink, 'dir')
-    .then(() => {
-      console.log('Successfully created symlink')
-    })
-    .catch(err => {
-      console.error(`error creating symlink`, err)
-    })
 })
