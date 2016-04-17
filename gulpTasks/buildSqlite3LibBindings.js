e2c8a5add95475dd0de2cbfc1aa8ca93971b0690
Coop <coop.coding@gmail.com>
2016-04-17 22:44:47 +1000

    Updated sqlite build task in gulp

--- gulpTasks/buildSqlite3LibBindings.js
+++ gulpTasks/buildSqlite3LibBindings.js
@@ -6,25 +6,28 @@ var gulp = require('gulp')
 var exeq = require('exeq')
 
 var platform = process.platform
+var electronVersion = require(path.resolve('', 'package.json')).dependencies['electron-prebuilt'].slice(1)
 
 gulp.task('sqlite', () => {
   // TODO - make this work on Windows & Linux
   var sqliteBinaryFullDir
 
   if(platform === 'darwin'){
+    /****
+     * Homebrew sqlite installed folder
+     * /usr/local/opt/sqlite/ is a symlink to the most up to date version installed in /usr/local/Cellar/sqlite
+     */
     sqliteBinaryFullDir = '/usr/local/opt/sqlite/'
   }
-  // else if(platform === 'darwin'){
+  // else if(platform === 'linux'){
   //
   // }
   // else if(platform === 'win32'){
   //
   // }
 
-  var electronVersion = require(path.resolve('', 'package.json')).dependencies['electron-prebuilt'].slice(1)
-
   return exeq(
-    `npm install sqlite3 --runtime=electron --target=${ electronVersion } --build-from-source --sqlite=${ sqliteBinaryFullDir }`
+    `npm install sqlite3 --runtime=electron --target=${ electronVersion } --target_arch=${ process.arch } --target_platform=${ platform } --build-from-source --sqlite=${ sqliteBinaryFullDir }`
   )
   .then(function() {
     console.log('Successfully compiled sqlite3 lib binding')
