6dd3250abe7d9245481f2a546efa32578a3fb856
Coop <coop.coding@gmail.com>
2016-04-22 19:23:36 +1000

    Cleaned up knexconfig

--- appmodules/db/knexConfig.js
+++ appmodules/db/knexConfig.js
@@ -4,10 +4,8 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      //filename: ':memory:'
       //dont put the filename here, we need to dynamically set it in appSettings.js and pagesdb.js
     },
-    //debug: (process.env.NODE_ENV === 'development'),
     /****
      * https://github.com/tgriesser/knex/pull/1043
      */
@@ -16,14 +14,7 @@ module.exports = {
   production: {
     client: 'sqlite3',
     connection: {
-      //filename: ':memory:'
-      //dont put the filename here, we need to dynamically set it in appSettings.js and pagesdb.js
     },
-    //debug: (process.env.NODE_ENV === 'development'),
     useNullAsDefault: false
-  },
-  testing: {
-    // client: 'pg',
-    // connection: process.env.DATABASE_URL
   }
 }
