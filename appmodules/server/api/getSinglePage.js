e6f65f0aaadb73dd799288c0753c3c713a8aac2f
Coop <coop.coding@gmail.com>
2016-08-06 11:16:34 +1000

    Tweaked getSinglePage so it doesn't return pageText

--- appmodules/server/api/getSinglePage.js
+++ appmodules/server/api/getSinglePage.js
@@ -4,22 +4,33 @@ var pagesdb = require('../../db/pagesdb')
 var appLogger = require('../../utils/appLogger')
 
 function getSinglePage(req, res) {
-  pagesdb.db('pages')
-      .where('pageUrl', req.params.pageUrl)
-      .first()
-      .then( row => {
-        if(!row){
-          res.status(404).end()
-        }
-        else{
-          res.json(row)
-        }
-      })
-      .catch( err => {
-        global.devMode && console.error(err)
-        appLogger.log.error({err, req, res})
-        res.status(500).end()
-      })
+  pagesdb
+    .db
+    .select(
+      'pageUrl',
+      'dateCreated',
+      'pageDomain',
+      'pageTitle',
+      'pageDescription',
+      'archiveLink',
+      'checkedForExpiry'
+    )
+    .from('pages')
+    .where('pageUrl', req.params.pageUrl)
+    .first()
+    .then( row => {
+      if(!row){
+        res.status(404).end()
+      }
+      else{
+        res.json(row)
+      }
+    })
+    .catch( err => {
+      global.devMode && console.error(err)
+      appLogger.log.error({err, req, res})
+      res.status(500).end()
+    })
 
 }
 
