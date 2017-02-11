c4f4be1d5485130f57c05347c2e1126249dac960
Darkle <coop.coding@gmail.com>
2017-02-12 09:41:21 +1100

    Put appdmg as an optional dependency

--- gulpTasks/bumpVersion.js
+++ gulpTasks/bumpVersion.js
@@ -4,8 +4,8 @@ var exeq = require('exeq')
 var moment = require('moment')
 
 gulp.task('bumpVersion', () => {
-  console.log(`running npm version --no-git-tag-version --force ${ moment().year() }.${ moment().month() + 1 }.${ moment().date() }`)
-  exeq(`npm version ${ moment().year() }.${ moment().month() + 1 }.${ moment().date() }`)
+  console.log(`running 'npm --no-git-tag-version --force version ${ moment().year() }.${ moment().month() + 1 }.${ moment().date() }'`)
+  exeq(`npm --no-git-tag-version --force version ${ moment().year() }.${ moment().month() + 1 }.${ moment().date() }`)
     .then(function() {
       console.log('Successfully compiled sqlite3 lib binding')
     })
