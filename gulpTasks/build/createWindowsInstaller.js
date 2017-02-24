97f88038a1e803e783ddb482236150284bfb770c
Darkle <coop.coding@gmail.com>
2017-02-24 17:03:44 +1100

    eslint stuff for createWindowsInstaller

--- gulpTasks/build/createWindowsInstaller.js
+++ gulpTasks/build/createWindowsInstaller.js
@@ -14,17 +14,17 @@ gulp.task('buildWindowsInstaller', () => {
 	console.log('Building windows installer')
   /*First we need to update the MarkSearch version in the .iss installer file*/
   LineDriver.write( {
-    in : issFilePath,
-    line : function( props, parser ){
+    in: issFilePath,
+    line: function(props, parser) {
       if(parser.line.startsWith('AppVersion=')){
-        parser.write(`AppVersion=${parser.line.split('AppVersion=')[0]}${marksearchVersion.version}`)
+        parser.write(`AppVersion=${ parser.line.split('AppVersion=')[0] }${ marksearchVersion.version }`)
       }
       else{
         parser.write(parser.line)
       }
     },
     /*write here is called when its done writing to the file, it's different to parser.write() */
-    write : function( props, parser ){
+    write: function() {
       console.log('finished changing version number in .iss file, now packaging into installer...')
       gulp.src(issFilePath).pipe(inno())
     }
